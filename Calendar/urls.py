a6a96feedf9b1e9cb4d7d9f00d30bf20673d4c72
jama <jamakazie@gmail.com>
2013-05-31 13:11:19 -0400

    list updates

--- Calendar/urls.py
+++ Calendar/urls.py
@@ -4,7 +4,10 @@ from Calendar import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.calendar_index, name='calendar_index'),
-	url(r'^add$', views.calendar_add, name='calendar_addd'),
+	url(r'^add$', views.calendar_add, name='calendar_add'),
+	url(r'^add/getrecipe$', views.calendar_ajax_getrecipe, name='calendar_ajax_getrecipe'),
 	url(r'^commit$', views.calendar_commit, name='calendar_commit'),
 	url(r'^view$', views.calendar_view, name='calendar_view'),
+	url(r'^grocerylist$', views.calendar_grocery_list, name='calendar_grocery_list'),
+	url(r'^grocerylist/generate$', views.calendar_ajax_grocerylist, name='calendar_ajax_grocerylist'),
 )
--- Calendar/views.py
+++ Calendar/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render, render_to_response
 import datetime
 from django.http import HttpResponse
@@ -40,9 +41,47 @@ def calendar_view(request):
 	context['events'] = events
 	return render_to_response('calendar_view.html', context, context_instance = RequestContext(request)) 
 
+def calendar_grocery_list(request):
+	context = {}
+	today = datetime.datetime.now();
+	next_week = today + datetime.timedelta(days=7)
+	context['today'] = today
+	context['next_week'] = next_week
+	return render_to_response('calendar_grocery_list.html', context, context_instance = RequestContext(request))
+
+def calendar_ajax_getrecipe(request):
+	try:
+		date = request.GET['date']
+		temp = datetime.datetime.strptime(date, '%m/%d/%Y')
+		recipe = id_to_name(c.objects.filter(date=temp)[0].recipe_id)
+		return HttpResponse(recipe)
+	except:
+		return HttpResponse("Nothing")
+
+def calendar_ajax_grocerylist(request):
+	#try:
+		start_date = request.GET['start_date']
+		end_date = request.GET['end_date']
+		f_start_date = datetime.datetime.strptime(start_date, '%m/%d/%Y').strftime('%Y-%m-%d')
+		f_end_date = datetime.datetime.strptime(end_date, '%m/%d/%Y').strftime('%Y-%m-%d')
+		events = c.objects.filter(date__range=[f_start_date, f_end_date])
+		items = []
+		for event in events:
+			recipe = r.objects.get(pk=event.recipe_id)
+			ingredients = json.loads(recipe.ingredients)
+			for ingredient in ingredients:
+				items.append(ingredient)
+		items = sorted(items, key=lambda k: k)
+		return HttpResponse(json.dumps(items))
+	#except:
+	#	return HttpResponse("shit fucked up :[")
+
+
+
 def name_to_id(name):
 	recipe = r.objects.filter(title=name)[0]
 	return recipe.pk
+
 def id_to_name(rid):
 	recipe = r.objects.filter(pk=rid)[0]
 	return recipe.title
